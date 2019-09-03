201872c4fc9112cf1754b43b401d1a716fb51f8c
Momade Abdul <momademha@gmail.com>
2019-09-03 18:09:55 +0200

    Primeira versao do Projecto com mensagem Ola Mundo!

---
+++ projecto-teste/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace projecto_teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
